3f56fe9b1c8d048da74e03f04f6a86f066f7bfc8
Nick <pacemkr@gmail.com>
2011-06-21 12:52:49 -0400

    Improved the test to check for multiple signals sent.

--- tests/test_any_signal.py
+++ tests/test_any_signal.py
@@ -21,21 +21,24 @@ def test_any_signal_listener():
 
 	some_signal1.send('test')
 	some_signal2.send('test', arg1='val1', arg2='val2')
+	some_signal1.send('test')
 	
 	print "output1 =>", output1
 	print "output2 =>", output2
 	print "outputall =>", outputall
 
-	assert len(output1) == 1
+	assert len(output1) == 2
 	assert output1[0]['signal'] == some_signal1
 	assert len(output2) == 1
 	assert output2[0]['signal'] == some_signal2
 	assert output2[0]['arg1'] == 'val1'
 	assert output2[0]['arg2'] == 'val2'
 
-	assert len(outputall) == 2
+	assert len(outputall) == 3
 	assert outputall[0]['signal'] == some_signal1
 	assert outputall[1]['signal'] == some_signal2
+	assert outputall[2]['signal'] == some_signal1
 	assert len(outputall[0]) == 1
 	assert outputall[1]['arg1'] == 'val1'
 	assert outputall[1]['arg2'] == 'val2'
+	assert len(outputall[2]) == 1
